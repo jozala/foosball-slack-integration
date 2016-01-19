60ca3a665b9c142fc5374fb73c94295ed9fafdad
Mariusz <mariusz@aetas.pl>
2016-01-19 21:31:02 +0100

    Fix to properly send "response_type" key to Slack More configuration moved to properties file Check Slack authentication token

--- src/main/kotlin/pl/aetas/slack/foos/Application.kt
+++ src/main/kotlin/pl/aetas/slack/foos/Application.kt
@@ -1,6 +1,8 @@
 package pl.aetas.slack.foos
 
+import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import pl.aetas.slack.foos.command.CommandParser
 import pl.aetas.slack.foos.command.CommandRunner
 import pl.aetas.slack.foos.mapping.UserMappingService
@@ -13,12 +15,18 @@ import java.util.*
 
 fun main(args : Array<String>) {
 
+    val gson: Gson = GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
 
-    val sslPassword = ApplicationProperties.get("ssl.password")
+    if (ApplicationProperties.isTrue("ssl.enabled")) {
+        val sslPassword = ApplicationProperties.get("ssl.password")
 
-    Spark.port(443);
-    Spark.secure("ssl/MyDSKeyStore.jks", sslPassword, null, null);
-    Spark.post("/integration", MyRoute(), ResponseTransformer { Gson().toJson(it) })
+        Spark.port(443);
+        Spark.secure("ssl/MyDSKeyStore.jks", sslPassword, null, null);
+    }
+
+    Spark.post("/integration", MyRoute(), ResponseTransformer { gson.toJson(it) })
 }
 
 object ApplicationProperties {
@@ -31,6 +39,7 @@ object ApplicationProperties {
     }
 
     fun get(key: String) = properties.getProperty(key)
+    fun isTrue(key:String) = properties.getProperty(key).equals("true", true)
 }
 
 class MyRoute: Route {
@@ -45,6 +54,13 @@ class MyRoute: Route {
         val channelId = request.queryParams("channel_id")
         val command = request.queryParams("text")
         val slackUsername = request.queryParams("user_name")
+
+        if (ApplicationProperties.isTrue("slack.token.required")) {
+            if (request.queryParams("token") != ApplicationProperties.get("slack.token")) {
+                response.status(401)
+                return "Incorrect Slack token";
+            }
+        }
 
         val commandRunner: CommandRunner = commandRunner(
                 channelId,
