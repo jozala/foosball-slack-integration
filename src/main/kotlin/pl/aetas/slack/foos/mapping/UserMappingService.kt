264e02a43945ac1868ed95084675c7bd660dcabe
Mariusz <mariusz@aetas.pl>
2016-01-20 00:25:43 +0100

    Persists user mapping data in JSON

--- src/main/kotlin/pl/aetas/slack/foos/mapping/UserMappingService.kt
+++ src/main/kotlin/pl/aetas/slack/foos/mapping/UserMappingService.kt
@@ -1,21 +1,44 @@
 package pl.aetas.slack.foos.mapping
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import pl.aetas.slack.foos.pushq.PushqSystem
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.FileReader
+import java.io.FileWriter
 
 class UserMappingService(private val pushqSystem: PushqSystem) {
 
-    // TODO - keep it persisted somewhere
-    val userMapBySlackUsername: MutableMap<String, String> = hashMapOf()
-    val userMapByPushqUsername: MutableMap<String, String> = hashMapOf()
+    val userMapBySlackUsername: MutableMap<String, String>
+    val userMapByPushqUsername: MutableMap<String, String>
+
+    val gson: Gson = Gson()
+    val type = object : TypeToken<Map<String, String>>() {}.type
+
+    init {
+        val fileReader = BufferedReader(FileReader("players_mapping.json"))
+
+        userMapBySlackUsername = gson.fromJson(fileReader, type)
+        userMapByPushqUsername = userMapBySlackUsername.map { Pair(it.value, it.key) }.toMap().toLinkedMap()
+        fileReader.close()
+    }
 
 
     fun addUserMapping(slackUsername: String, pushqUsername: String) {
         if (pushqSystem.users().contains(pushqUsername)) {
             userMapBySlackUsername.put(slackUsername, pushqUsername)
             userMapByPushqUsername.put(pushqUsername, slackUsername)
+            storeUserMap(userMapBySlackUsername)
         } else {
             throw UnknownPushqUsername("Username: $pushqUsername has not been found in PushQ's system")
         }
+    }
+
+    private fun storeUserMap(userMapBySlackUsernameToStore: Map<String, String>) {
+        val mappingFile = BufferedWriter(FileWriter("players_mapping.json"))
+        gson.toJson(userMapBySlackUsernameToStore, type, mappingFile)
+        mappingFile.close()
     }
 
     fun getPlayerBySlackUsername(slackUsername: String): Player? {
