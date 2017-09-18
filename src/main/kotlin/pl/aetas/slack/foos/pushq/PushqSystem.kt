4a4cac622a5c121b139398548bc0a5c7557f9c93
Mariusz <mariusz@aetas.pl>
2017-09-19 00:27:04 +0200

    Correction to fetch ranking back as it was before (but not the users/players)

--- src/main/kotlin/pl/aetas/slack/foos/pushq/PushqSystem.kt
+++ src/main/kotlin/pl/aetas/slack/foos/pushq/PushqSystem.kt
@@ -10,6 +10,8 @@ open class PushqSystem {
     val gson: Gson = Gson()
 
     val PUSHQ_URL_API_PLAYERS = ApplicationProperties.get("pushq.url.api.players")
+    val PUSHQ_URL_API_LEMSTAT = ApplicationProperties.get("pushq.url.api.lemstat")
+
 
     open fun users(): List<String> {
         val response = URL(PUSHQ_URL_API_PLAYERS).readText()
@@ -17,8 +19,11 @@ open class PushqSystem {
         return stats.map { it.get("name")!! }
     }
 
+    open fun ranking(): List<String> {
+        val response = URL(PUSHQ_URL_API_LEMSTAT).readText()
+        val stats: ArrayList<Map<String, String>> = gson.fromJson(response, typeLiteral<ArrayList<Map<String, String>>>())
+        return stats.map { it.get("name")!! }
+    }
+
     inline private fun <reified T: List<Map<String, String>>> typeLiteral() = T::class.java
-
-
-    open fun ranking(): List<String> = users()
 }
